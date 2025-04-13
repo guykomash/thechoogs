95a6f01190f269f876b81bca2c8474e29acbe07d
Guy Komash <gkomash@gmail.com>
2025-04-13 03:51:40 +0300

    basic frontend and configure lint and prettier

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 import Fastify from "fastify";
+import fastifyStatic from "@fastify/static";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const fastify = Fastify({
-    logger: true
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const fastify = Fastify({ logger: true });
+
+fastify.register(fastifyStatic, {
+  root: path.join(__dirname, "public"),
 });
 
-fastify.get('/', async function handler (request, reply) {
-    return {
-        hello: 'world'
-    }
-})
-
+fastify.get("/", async (req, reply) => {
+  return reply.sendFile("views/index.html"); // served from /public
+});
 
 try {
-
-    await fastify.listen({port: 3000})
+  await fastify.listen({ port: 3000 });
+  console.log("Server running at http://localhost:3000");
 } catch (err) {
-    fastify.log.error(err);
-    process.exit(1);
+  fastify.log.error(err);
+  process.exit(1);
 }
