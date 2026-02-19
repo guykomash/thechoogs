c935602c7bd2a17c53459382f704b09f3d1e7cae
Guy Komash <128089503+guykomash@users.noreply.github.com>
2026-02-19 20:54:11 +0200

    Revert "custom url"

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://thechoogs.com",
+  site: "https://guykomash.github.io",
+  base: "/blog",
 });
